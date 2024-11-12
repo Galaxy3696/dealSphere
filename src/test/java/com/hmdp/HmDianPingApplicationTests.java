45e04eee37c534b1ef746dbdbfd01c4000d58125
msy <mengsy01@gmail.com>
2024-11-12 09:55:39 -0800

    redis实现全局唯一ID

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,37 @@
 package com.hmdp;
 
+import com.hmdp.utils.RedisIdWorker;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Resource
+    RedisIdWorker redisIdWorker;
 
+    private ExecutorService es = Executors.newFixedThreadPool(500 );
+
+    @Test
+    void testIdWorker () throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(300);
+        Runnable task = () ->{
+            for (int i = 0; i < 100; i++) {
+                Long id = redisIdWorker.nextId("order");
+                System.out.println(id);
+            }
+            countDownLatch.countDown();
+        };
+        Long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++)
+            es.submit(task);
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 }
